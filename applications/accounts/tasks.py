7103cd5fb4c45d143559ceaa544b4d03f2610fa0
Ваше Имя <you@example.com>
2024-02-06 11:20:54 +0600

    added celery

---
+++ applications/accounts/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+@shared_task
+def send_custom_email_task(email, subject, template_name, context):
+    html_message = render_to_string(template_name, context)
+    send_mail(
+        subject,
+        None,
+        'kalmanbetovnurislam19@gmail.com', 
+        [email],
+        html_message=html_message,
+        fail_silently=False,
+    )
+    print("Письмо отправлено")
